cbf473079a9c6bede4703f0292004bf31c2d5224
Amirn88 <eng.ameer.elngaar88@gmail.com>
2025-05-31 23:34:19 +0300

    Added Helm-Chart

--- Dockerfile-Python-file/app.py
+++ Dockerfile-Python-file/app.py
@@ -1,12 +1,20 @@
 from flask import Flask
+from prometheus_client import generate_latest, Counter, Gauge, REGISTRY
 
 app = Flask(__name__)
 
+REQUEST_COUNT = Counter('http_requests_total', 'Total HTTP Requests')
+RANDOM_VALUE = Gauge('random_value', 'Randomly generated value')
+
 @app.route('/')
-def hello():
-    return "Welcome to DevOps team Nginx with Python !"
+def home():
+    REQUEST_COUNT.inc()
+    return "Hello from metrics app!"
+
+@app.route('/metrics')
+def metrics():
+    RANDOM_VALUE.set(REQUEST_COUNT._value.get() % 10)  # Simulate changing metric
+    return generate_latest(REGISTRY)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
-
-
+    app.run(host='0.0.0.0', port=5000)
